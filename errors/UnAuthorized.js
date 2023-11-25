c488154587a55d7b958ba727e95b1f150b98bd12
Osher Solimany <solimani.osher@gmail.com>
2023-11-25 16:44:20 +0200

    Add StatusCodes from http-status-codes npm package

--- errors/UnAuthorized.js
+++ errors/UnAuthorized.js
@@ -1,9 +1,11 @@
+const { StatusCodes } = require('http-status-codes');
+
 const CustomAPIError = require('./CustomAPIError');
 
 class UnAuthorized extends CustomAPIError {
   constructor(message) {
     super(message);
-    this.statusCode = 401;
+    this.statusCode = StatusCodes.UNAUTHORIZED;
   }
 }
 
